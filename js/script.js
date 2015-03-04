463f272a43ae21f0531d4f551fe2104324d5db8d
pcsandford <petersandford1982@gmail.com>
2015-03-04 16:02:20 -0500

    changes to QR code positioning

--- js/script.js
+++ js/script.js
@@ -95,21 +95,21 @@ $(document).ready(function() {
 
 		//QR code button animation
 
-	var closeButton = function(){
-		$('.QRcode').removeClass('slideAnimation').addClass('slideAnimationDown');
-	}	
+	// var closeButton = function(){
+	// 	$('.QRcode').removeClass('slideAnimation').addClass('slideAnimationDown');
+	// }	
 
-	var closeButtonTimer;
+	// var closeButtonTimer;
 	
-    $('button').on('click',function(){
-    if ($('.QRcode').hasClass('slideAnimation')){
-    	$('.QRcode').removeClass('slideAnimation').addClass('slideAnimationDown');
-    	clearTimeout(closeButtonTimer);
-    } else {
-    	$('.QRcode').removeClass('slideAnimationDown').addClass('slideAnimation');
-    	closeButtonTimer = setTimeout(closeButton,12000);
-    }
-    });
+ //    $('button').on('click',function(){
+ //    if ($('.QRcode').hasClass('slideAnimation')){
+ //    	$('.QRcode').removeClass('slideAnimation').addClass('slideAnimationDown');
+ //    	clearTimeout(closeButtonTimer);
+ //    } else {
+ //    	$('.QRcode').removeClass('slideAnimationDown').addClass('slideAnimation');
+ //    	closeButtonTimer = setTimeout(closeButton,12000);
+ //    }
+ //    });
  
 
 	 });
